0e56d6077b40f54e54a751997c48f96830557c5b
Davide Giacomini <davide.giacomini2@studio.unibo.it>
2025-08-30 20:15:36 +0200

    removed CorsConfiguration, appears to not be required improved logging filter changed com.example package logging level added logging configuration in gateway configuration in docker compose

--- gateway/src/main/java/com/example/gateway/LoggingConfiguration.java
+++ gateway/src/main/java/com/example/gateway/LoggingConfiguration.java
@@ -1,32 +1,37 @@
 package com.example.gateway;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.springframework.cloud.gateway.filter.GatewayFilterChain;
-import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.server.ServerWebExchange;
-
-import java.util.Collections;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
 
 import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.*;
 
 @Configuration
 public class LoggingConfiguration {
-    private final Log log = LogFactory.getLog(getClass());
+    private static final Logger log = LoggerFactory.getLogger(LoggingConfiguration.class);
 
     @Bean
-    public GlobalFilter getLoggingFilter() {
-        return (ServerWebExchange exchange, GatewayFilterChain chain) -> {
-            final var uris = exchange.getAttributeOrDefault(GATEWAY_ORIGINAL_REQUEST_URL_ATTR, Collections.emptySet());
-            final var originalUri = (uris.isEmpty()) ? "Unknown" : uris.iterator().next().toString();
-            final var route = exchange.getAttribute(GATEWAY_ROUTE_ATTR);
+    public WebFilter getLoggingFilter() {
+        return (ServerWebExchange exchange, WebFilterChain chain) -> {
+            final var logPrefix = exchange.getLogPrefix();
+            final var request = exchange.getRequest();
+
+            final var requestMethod = request.getMethod().toString();
+            final var requestUri = request.getURI().toString();
+
+            final var routeId = exchange.getAttribute(GATEWAY_ROUTE_ATTR);
             final var routeUri = exchange.getAttribute(GATEWAY_REQUEST_URL_ATTR);
 
-            log.info("Incoming request " + originalUri + " is routed to id: " + route + ", uri:" + routeUri);
+            log.debug("{}Incoming {} request {} is routed to {} (id: {}, headers: {})", logPrefix, requestMethod, requestUri, routeUri, routeId,                     exchange.getRequest().getHeaders().entrySet().stream()
+                    .filter(stringListEntry -> !stringListEntry.getKey().equals("Authorization")).toList());
 
-            return chain.filter(exchange);
+            return chain.filter(exchange).doFinally((signalType) -> {
+                log.info("{}Fulfilling request with status {} (signal type: {})", logPrefix, exchange.getResponse().getStatusCode(), signalType);
+            });
         };
     }
 }
